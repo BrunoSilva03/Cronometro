b2763748c12a147d9e3ac479704e016758c2d0cf
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-26 15:39:00 -0300

    adicionando minutos e segundos

---
+++ script.js
@@ -0,0 +1,41 @@
+let buttonIniciar = window.document.getElementById('iniciar');
+let buttonFinalizar = window.document.getElementById('finalizar');
+
+buttonIniciar.addEventListener('click', iniciarContagem);
+buttonFinalizar.addEventListener('click', finalizarContagem);
+
+var minutos = window.document.getElementById('minutos');
+function iniciarContagem() {
+
+    tempo = setInterval(function() {
+        var segundos = window.document.getElementById('segundos').innerHTML;
+        var soma = parseInt(segundos) + 1;
+
+        if(soma >= 60) {
+            soma = 0;
+            maisUmMinuto();
+        }
+
+        window.document.getElementById('segundos').innerHTML = soma;
+    },1);
+    
+    /*
+    setInterval(function() {
+        var minutos = window.document.getElementById('minutos').innerHTML;
+        var soma = parseInt(cronometro) + 1;
+
+        
+        window.document.getElementById('minutos').innerHTML = soma;
+    }, 1000)
+    */
+    
+}
+
+function maisUmMinuto() {
+    
+}
+
+
+function finalizarContagem() {
+    clearInterval(tempo);
+}
