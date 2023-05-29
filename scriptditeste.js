b06a192982cc26763c76507fc4e8ebf951f8ad18
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-29 12:08:44 -0300

    Finalizando os segundos

--- scriptditeste.js
+++ scriptditeste.js
@@ -2,27 +2,38 @@ let buttonIniciar = window.document.getElementById('iniciar');
 let buttonFinalizar = window.document.getElementById('finalizar');
 
 timer = window.document.getElementById('timer').innerHTML;
-buttonIniciar.addEventListener('click', iniciarContagem(timer));
+buttonIniciar.addEventListener('click', iniciarContagem);
 buttonFinalizar.addEventListener('click', finalizarContagem);
 
 
 
-function iniciarContagem(timer) {
-    setInterval(function() {
+function iniciarContagem() {
+    setInterval(function () {
 
         var soma = parseInt(timer) + 1;
+        var minutos = false;
 
-        if(soma < 10) {
+        if (soma < 10 && minutos == false) {
             window.document.getElementById('timer').innerHTML = '00:0';
-            var zeroAntes = window.document.getElementById('timer').innerHTML;
-            //zeroAntes = '0';
-            zeroAntes += soma;
-            /*
-            window.document.getElementById('timer').innerHTML += zeroAntes;
-            */
-            timer = window.document.getElementById('timer').innerHTML
-
             window.document.getElementById('timer').innerHTML += soma;
+            timer = soma
+
+        }
+
+        if(soma >= 10 && minutos == false) {
+            window.document.getElementById('timer').innerHTML = '00:';
+            window.document.getElementById('timer').innerHTML += soma;
+            timer = soma;
+        }
+
+        if(soma >= 60 && minutos == false) {
+            window.document.getElementById('timer').innerHTML = '01:00';
+            soma = 0;
+            timer = soma;
+            acrescentaMinutos();
+
         }
     }, 1000)
 }
+
+
